60791d3a175b72569eb9e378a63fa6d77c431817
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-09 16:06:27 -0500

    Exercises complete. Codewars 1 complete

--- codewars.js
+++ codewars.js
@@ -2,7 +2,19 @@
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
-
+var number=function(array){
+  
+    if (array.length == 0){
+      return array;
+    }
+    
+    for (let i = 0; i < array.length; i++){
+      array[i] = parseInt(i + 1) + ": " + array[i];
+    }
+    
+    return array;
+      
+  }
 
 
 // # 2
